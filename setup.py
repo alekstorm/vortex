61dc346c7390d24a0b75d40c256a0fb5a0486de6
Alek Storm <alek.storm@gmail.com>
2011-08-24 19:37:16 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import distutils.core
+import sys
+# Importing setuptools adds some features like "setup.py develop", but
+# it's optional so swallow the error if it's not there.
+try:
+    import setuptools
+except ImportError:
+    pass
+
+kwargs = {}
+
+major, minor = sys.version_info[:2]
+
+if major >= 3:
+    import setuptools  # setuptools is required for use_2to3
+    kwargs["use_2to3"] = True
+
+distutils.core.setup(
+    name="vortex",
+    py_modules = ["vortex"],
+    author="Alek Storm",
+    author_email="alek.storm@gmail.com",
+    url="http://alekstorm.github.com/vortex",
+    **kwargs
+)
